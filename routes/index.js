619ff3b13e1eaa093aa626977912b7fb419482a7
jdpatton44 <jdpattonv@gmail.com>
2020-08-17 15:43:07 -0400

    working on ftp connection

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const upload = require('../middlewares/upload');
 const imbController = require('../controllers/imbController');
+const ftp = require('../middlewares/ftpClient')
 // const { catchErrors } = require('../handlers/errorHandlers');
 
 const router = express.Router();
 
 router.post('/upload-file', upload.single('file'), imbController.uploadFile, imbController.exportTrackingFileToDB);
+
+router.get('/checkFtp', imbController.getFileFromFTP);
 
 module.exports = router;
